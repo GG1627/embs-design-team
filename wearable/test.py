744e02afaa20579971defbed3198b5903732f56c
Gael Garcia <gael.garcia@ufl.edu>
2026-04-20 19:42:26 -0400

    updates

--- wearable/test.py
+++ wearable/test.py
@@ -1,18 +1,30 @@
 import asyncio
 from bleak import BleakClient, BleakScanner
 
-ADDRESS = "40:22:D8:75:0F:2A"
+NAME = "BioMonitor_ESP32"
 TX_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 RX_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 
-def on_notify(_, data):
-    print("notify:", data.decode())
+def on_notify(sender, data):
+    try:
+        print("Notify:", data.decode())
+    except:
+        print("Notify bytes:", data)
 
 async def main():
-    async with BleakClient(ADDRESS) as client:
-        print("connected:", client.is_connected)
+    device = await BleakScanner.find_device_by_name(NAME, timeout=10.0)
+    if not device:
+        print("ESP32 not found")
+        return
+
+    async with BleakClient(device, timeout=20.0) as client:
+        print("Connected:", client.is_connected)
         await client.start_notify(TX_UUID, on_notify)
-        await client.write_gatt_char(RX_UUID, b"hello from pi")
-        await asyncio.sleep(30)
+
+        await client.write_gatt_char(RX_UUID, b"hello esp32")
+        print("Sent message")
+
+        while True:
+            await asyncio.sleep(1)
 
 asyncio.run(main())
